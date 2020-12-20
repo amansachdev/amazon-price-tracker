4d6447a0965f82001a85473622a1311fd3aca9cd
amansachdev <46645259+amansachdev@users.noreply.github.com>
2020-12-20 16:14:11 +0530

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -24,7 +24,7 @@ from datetime import date, datetime, timedelta
 #iplist = ['162.208.48.84:8118','165.138.4.41:8080']
 #proxies = {'https':random.choice(iplist) }
 #print(proxies)
-client = ScraperAPIClient('32b2027a1cd8ca5bdc4d01bf3b5db1ef')
+client = ScraperAPIClient('3884e683f7c5972801bf01823f3d0fae')
 ua = UserAgent.UserAgent()
 #imported_proxy = random_proxy.random_proxies()
 #print(imported_proxy)
